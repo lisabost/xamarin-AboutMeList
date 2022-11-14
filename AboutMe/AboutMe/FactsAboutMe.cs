f78e016d3faec50bdeecffd60f4fb7b54f882a95
Lisa Bost <bost.lisam@gmail.com>
2022-11-14 13:32:20 -0600

    Add project files.

---
+++ AboutMe/AboutMe/FactsAboutMe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AboutMe
+{
+    public class FactsAboutMe
+    {
+        public FactsAboutMe()
+        { }
+
+        public static IEnumerable<FactsAboutMe> Facts { private set; get; }
+
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        public string ImageSource { get; set; }
+
+        static FactsAboutMe()
+        {
+            List<FactsAboutMe> facts = new List<FactsAboutMe>();
+            facts.Add(new FactsAboutMe() { TheFact = "Sunflowers are my favorite flower", ShortFact = "Sunflowers", ImageSource = "sunflower.png"});
+            facts.Add(new FactsAboutMe() { TheFact = "I have 8 siblings", ShortFact = "Siblings", ImageSource = "siblings.png" });
+            facts.Add(new FactsAboutMe() { TheFact = "I was born and raised in California", ShortFact = "California", ImageSource = "california.png" });
+            facts.Add(new FactsAboutMe() { TheFact = "Autumn is my favorite season", ShortFact = "Autumn", ImageSource = "leaves.png" });
+            facts.Add(new FactsAboutMe() { TheFact = "I like to play video games", ShortFact = "Spare Time", ImageSource = "videogames.png" });
+            facts.Add(new FactsAboutMe() { TheFact = "I enjoy crocheting because it keeps my hands busy", ShortFact = "Hobby", ImageSource = "crochet.png" });
+            Facts = facts;
+        }
+
+    }
+}
---
+++ AboutMe/AboutMe/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AboutMe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            FactsAboutMe fact = (FactsAboutMe)e.SelectedItem;
+            DisplayAlert("The Fact", fact.TheFact, "Ok");
+        }
+    }
+}
